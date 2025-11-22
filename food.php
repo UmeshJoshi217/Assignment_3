5193a6e7940d20fe5de3977b1fd21fd32adf8122
UmeshJoshi217 <umeshjoshi938@gmail.com>
2025-11-22 13:04:36 +0545

    Add files via upload

---
+++ food.php
@@ -0,0 +1,11 @@
+<?php
+$food = array["Momo", "Chowmin", "Pizza", "Pasta", "Rice"];
+$i=1;
+foreach($food as $x){
+    echo $i. "" . $x ;
+    $i++;
+}
+
+$cities = $cities = ['Kathmandu', 'Pokhara', 'Lalitpur', 'Bhaktapur', 'Biratnagar'];
+
+?>
