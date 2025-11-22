7355abb118cbff3908792dc95cb91e73fdf97076
UmeshJoshi217 <umeshjoshi938@gmail.com>
2025-11-22 13:14:26 +0545

    Add introduce function to display personal information

---
+++ person.php
@@ -0,0 +1,10 @@
+<?php
+function introduce($name, $age, $city){
+    echo "My name is $name.<br>";
+    echo "I am $age years old.<br>";
+    echo "I live in $city.<br><br>";
+}
+introduce("Ram Sharma", 20, "Kathmandu");
+introduce("Sita Rai", 19, "Pokhara");
+introduce("Hari Gurung", 21, "Lalitpur");
+?>
